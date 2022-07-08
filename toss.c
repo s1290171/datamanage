1f3f2d43d40ec694784a2d626915498c151d87fe
s1290171 <s1290171@std5dc10.u-aizu.ac.jp>
2022-07-08 12:51:49 +0900

    HoT: initial revision

---
+++ toss.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(){
+  int judge;
+  int round = 1,head = 0,tail = 0;
+  int i;
+
+  srand(time(NULL));
+  
+  printf("Tossing a coin...\n");
+  
+  for(i = 0; i < 3; i++){
+    judge = rand();
+    printf("%d\n",judge);
+    
+    if( judge % 2 == 0){
+      printf("Round %d: Heads\n",round);
+      round++;
+      head++;
+    }
+    else{
+      printf("Round %d: Tails\n",round);
+      round++;
+      tail++;
+    }
+  }
+
+  printf("Heads: %d, Tails: %d\n",head,tail);
+    
+  return 0;
+}
